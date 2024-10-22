be91d9322311f2cdb026ef7c0a7abd16303eb9f8
BrayhanRZC <brayhan.riaza@julius2grow.com>
2024-10-22 15:28:17 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
-let form = document.querySelector('.form-register');
+let form = document.getElementById('form-register');
 let progressOptions = document.querySelectorAll('.progressbar__option');
 
 form.addEventListener('click', function(e) {
     let el = e.target;
     let isButtonBack = el.classList.contains('step__button--back');
     let isButtonNext = el.classList.contains('step__button--next');
+    
     if (isButtonBack || isButtonNext) {
         let currentStep = document.getElementById('step-' + el.dataset.step);
         let jumpStep = document.getElementById('step-' + el.dataset.to_step);
+        
         currentStep.addEventListener('animationend', function callback() {
             currentStep.classList.remove('active');
             jumpStep.classList.add('active');
@@ -20,82 +22,55 @@ form.addEventListener('click', function(e) {
             }
             currentStep.removeEventListener('animationend', callback);
         });
+        
         currentStep.classList.add('inactive');
         jumpStep.classList.remove('inactive');
     }
 });
 
-function Enviar() {
-    let container = document.getElementById('form-register');
-    if (pageIsValidContent(container)) {
-        let myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("groupId", "70");
+let submitBtn = document.querySelector('.step__button[type="submit"]'); // Selecciona el botón de envío
 
-        let formulario = JSON.stringify({
-            "Email": document.getElementById("email").value,
-            "Name": document.getElementById("nombre").value,
-            "MobilePrefix": "57",
-            "Fields": [
-                {
-                    "Id": 1,
-                    "Name": "NombreUsuario",
-                    "Value": document.getElementById("nombre").value
-                },
-                {
-                    "Id": 20,
-                    "Name": "PreguntaDulce",
-                    "Value": document.querySelector('input[name="pregunta1"]:checked').value
-                },
-                {
-                    "Id": 31,
-                    "Name": "TipoLinea",
-                    "Value": document.querySelector('input[name="pregunta2"]:checked').value
-                },
-                {
-                    "Id": 38,
-                    "Name": "FraseLorenzano",
-                    "Value": document.querySelector('input[name="pregunta3"]:checked').value
-                },
-                {
-                    "Id": 7,
-                    "Name": "aceptaTerminos",
-                    "Value": 'Si'
-                },
-                {
-                    "Id": 8,
-                    "Name": "aceptaContacto",
-                    "Value": 'Si'
-                },
-                {
-                    "Id": 30,
-                    "Name": "Campaña",
-                    "Value": 'Concurso Halloween'
-                },
-                {
-                    "Id": 29,
-                    "Name": "Marca",
-                    "Value": 'Lorenzano'
-                }
-            ]
-        });
+submitBtn.addEventListener('click', function(e) {
+    e.preventDefault(); // Evita el envío por defecto del formulario
 
-        let requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: formulario,
-            redirect: 'follow'
-        };
+    let formData = new FormData(form); // Usa el formulario completo
+    let fechaHoraActual = new Date();
+    formData.append('fecha', fechaHoraActual.toLocaleDateString());
+    formData.append('hora', fechaHoraActual.toLocaleTimeString());
 
-        fetch("https://hook.us2.make.com/m8ebk2g5wq9q6jvszwj3kshqomwdm38r", requestOptions)
-            .then(response => response.text())
-            .then(result => {
-                console.log(result);
-                location.href = "https://www.lorenzano.co/halloween-gracias-concurso";
-            })
-            .catch(error => {
-                console.log('error', error);
-                location.href = "https://www.lorenzano.co/error-404";
-            });
-    }
+    mostrarPopup(); // Muestra el popup de carga
+
+    fetch('https://hook.us1.make.com/cxndb4giflcolhx6uovzqqe3ttoqv3ds', { // Cambia esto a tu URL de webhook
+        method: 'POST',
+        body: formData
+    })
+    .then(function(response) {
+        if (response.ok) {
+            console.log('Envío exitoso');
+            window.location.href = "https://www.lorenzano.co/halloween-gracias-concurso"; // Redirigir en caso de éxito
+        } else {
+            console.log('No enviado');
+            mostrarErrorPopup(); // Muestra popup de error
+        }
+    })
+    .catch(function(error) {
+        console.error('Error:', error);
+        mostrarErrorPopup(); // Muestra popup de error
+    });
+});
+
+function mostrarPopup() {
+    document.querySelector('.overlay').style.display = 'block';
+    document.querySelector('.spinner-popup').style.display = 'block';
+    setTimeout(function() {
+        document.querySelector('.overlay').style.display = 'none';
+        document.querySelector('.spinner-popup').style.display = 'none';
+    }, 6000); // Muestra el spinner por 6 segundos
+}
+
+function mostrarErrorPopup() {
+    document.querySelector('.error-popup').style.display = 'block';
+    setTimeout(function() {
+        document.querySelector('.error-popup').style.display = 'none';
+    }, 2000); // Oculta el popup de error después de 2 segundos
 }
