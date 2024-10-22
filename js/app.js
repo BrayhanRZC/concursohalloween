da088f5880dd599dfd135836a8e7ddfede813b95
BrayhanRZC <brayhan.riaza@julius2grow.com>
2024-10-22 15:54:45 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,94 +1,76 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let pasoActual = 1;
+let form = document.querySelector('.form-register');
+let progressOptions = document.querySelectorAll('.progressbar__option');
 
-    document.querySelectorAll('.step__button--next').forEach(button => {
-        button.addEventListener('click', function() {
-            if (pasoActual < 4) {
-                if (document.querySelectorAll('input[name=paso' + pasoActual + ']:checked').length === 0) {
-                    mostrarErrorPopup();
-                    return;
-                }
-                document.querySelectorAll('.step').forEach(step => step.style.display = 'none');
-                document.getElementById('step-' + (pasoActual + 1)).style.display = 'block';
-                document.querySelector('.step__button--back').style.display = 'block';
-                document.querySelectorAll('.progressbar__option.active')[0].classList.remove('active');
-                document.querySelectorAll('.progressbar__option')[pasoActual].classList.add('active');
-                pasoActual++;
-
-                // Animación GSAP
-                gsap.from('#step-' + pasoActual, { opacity: 0, duration: 0.5 });
-            }
-            if (pasoActual === 4) {
-                document.querySelectorAll('.step__button--next').forEach(btn => btn.style.display = 'none');
-                document.querySelector('.step__button[type="submit"]').style.display = 'block';
-            }
-        });
-    });
-
-    document.querySelector('.step__button--back').addEventListener('click', function() {
-        if (pasoActual > 1) {
-            document.querySelectorAll('.step').forEach(step => step.style.display = 'none');
-            document.getElementById('step-' + (pasoActual - 1)).style.display = 'block';
-            document.querySelector('.step__button[type="submit"]').style.display = 'none';
-            document.querySelectorAll('.step__button--next')[0].style.display = 'block';
-            document.querySelectorAll('.progressbar__option.active')[0].classList.remove('active');
-            document.querySelectorAll('.progressbar__option')[pasoActual - 2].classList.add('active');
-            pasoActual--;
-
-            // Animación GSAP
-            gsap.from('#step-' + pasoActual, { opacity: 0, duration: 0.5 });
-        }
-        if (pasoActual === 1) {
-            document.querySelector('.step__button--back').style.display = 'none';
-        }
-    });
-
-    document.getElementById('form-register').addEventListener('submit', function(e) {
-        e.preventDefault(); // Evita el envío por defecto del formulario
-
-        let formData = new FormData(this); // Usa el formulario completo
-        let fechaHoraActual = new Date();
-        formData.append('fecha', fechaHoraActual.toLocaleDateString());
-        formData.append('hora', fechaHoraActual.toLocaleTimeString());
-
-        // Mostrar el popup con el spinner y el overlay
-        mostrarPopup();
-
-        // Aquí puedes enviar los datos utilizando Fetch API a tu webhook
-        fetch('https://hook.us2.make.com/m8ebk2g5wq9q6jvszwj3kshqomwdm38r', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => {
-            if (response.ok) {
-                console.log('Envío exitoso');
-                window.location.href = 'https://www.lorenzano.co/halloween-gracias-concurso'; // Redirigir en caso de éxito
+// Manejador de los botones para avanzar y retroceder entre pasos
+form.addEventListener('click', function(e) {
+    let el = e.target;
+    let isButtonBack = el.classList.contains('step__button--back');
+    let isButtonNext = el.classList.contains('step__button--next');
+    if (isButtonBack || isButtonNext) {
+        let currentStep = document.getElementById('step-' + el.dataset.step);
+        let jumpStep = document.getElementById('step-' + el.dataset.to_step);
+        currentStep.addEventListener('animationend', function callback() {
+            currentStep.classList.remove('active');
+            jumpStep.classList.add('active');
+            if (isButtonNext) {
+                currentStep.classList.add('to-left');
+                progressOptions[el.dataset.to_step - 1].classList.add('active');
             } else {
-                console.log('No enviado');
-                mostrarErrorPopup(); // Muestra popup de error
+                jumpStep.classList.remove('to-left');
+                progressOptions[el.dataset.step - 1].classList.remove('active');
             }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            mostrarErrorPopup(); // Muestra popup de error
+            currentStep.removeEventListener('animationend', callback);
         });
-    });
-
-    function mostrarPopup() {
-        document.querySelector('.overlay').style.display = 'block';
-        document.querySelector('.spinner-popup').style.display = 'block';
-        // Ocultar el popup y el overlay después de 6 segundos
-        setTimeout(function() {
-            document.querySelector('.overlay').style.display = 'none';
-            document.querySelector('.spinner-popup').style.display = 'none';
-        }, 6000);
-    }
-
-    function mostrarErrorPopup() {
-        const errorPopup = document.querySelector('.error-popup');
-        errorPopup.style.display = 'block';
-        setTimeout(function() {
-            errorPopup.style.display = 'none';
-        }, 2000);
+        currentStep.classList.add('inactive');
+        jumpStep.classList.remove('inactive');
     }
 });
+
+// Enviar la información al webhook
+function Enviar() {
+    let email = document.getElementById("email").value;
+    let nombre = document.getElementById("nombre").value;
+    let pregunta1 = document.querySelector('input[name="paso1"]:checked').value;
+    let pregunta2 = document.querySelector('input[name="paso2"]:checked').value;
+    let pregunta3 = document.querySelector('input[name="paso3"]:checked').value;
+
+    // Verifica que los campos no estén vacíos
+    if (nombre && email && pregunta1 && pregunta2 && pregunta3) {
+        let myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        // Crear el JSON con la información
+        let formulario = JSON.stringify({
+            "Email": email,
+            "Name": nombre,
+            "Fields": [
+                { "Id": 1, "Name": "NombreUsuario", "Value": nombre },
+                { "Id": 2, "Name": "Pregunta1", "Value": pregunta1 },
+                { "Id": 3, "Name": "Pregunta2", "Value": pregunta2 },
+                { "Id": 4, "Name": "Pregunta3", "Value": pregunta3 }
+            ]
+        });
+
+        let requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: formulario,
+            redirect: 'follow'
+        };
+
+        // Envía los datos al webhook
+        fetch("https://tu-webhook-url.com", requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                console.log(result);
+                alert("¡Registro completado exitosamente!");
+            })
+            .catch(error => {
+                console.log('error', error);
+                alert("Hubo un error. Inténtalo de nuevo.");
+            });
+    } else {
+        alert("Por favor, completa todos los campos y selecciona una opción en cada pregunta.");
+    }
+}
+
